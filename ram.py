280ef06ccb52723a7bbf5a34a1e0e6f05384665f
ramachandran8270 <101502857+ramachandran8270@users.noreply.github.com>
2022-08-11 17:41:18 +0530

    first file

---
+++ ram.py
@@ -0,0 +1,9 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+name=input("enter your name : ")
+enter your name : 
+age=int(input("enter your age : ")
+
+if age>=18
+        print("name : ", 'eligible for vote')
+        
